6194326d79ce3f7a0d9a0eab9f42799d41bd6033
asusrid <alexsurid@gmail.com>
2023-11-27 10:09:24 +0100

    api POST call and wait func

--- sensors/sensorA.js
+++ sensors/sensorA.js
@@ -7,6 +7,33 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+function wait(time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+async function push() {
+  while (true) {
+    await wait(5000);
+
+    await axios
+      .post('http://localhost:5000/sensor', {
+        data: {
+          sensorId: 'sensorA',
+          siteId: 'siteA',
+          value: Math.floor(Math.random() * 1500) + 1,
+          timestamp: Date.now(),
+        },
+      })
+      .catch((err) => {
+        console.log(err.message);
+        console.log(err.response.data);
+      });
+
+    console.log('This printed after about 5 seconds');
+  }
+}
+
 app.listen(4001, () => {
+  push();
   console.log('Sensor A listening on port 4001...');
 });
